b099384118b1338460a43b42d8649a3d1a7638a7
kendallha <kendallhaw@gmail.com>
2021-02-21 14:03:53 -0800

    Refactor code for readability

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-var mantraButton = document.querySelector('#mantra');
 var affButton = document.querySelector('#affirmation');
-var receiveMsgBtn =  document.querySelector('#receive-message');
-var message = document.querySelector('#message');
 var bellIcon = document.querySelector('#bell');
-var loginButton = document.querySelector('#login-button');
-var nameInput = document.querySelector('#login-box');
-var mainPage = document.querySelector('.main-page');
-var loginPage = document.querySelector('.login-page');
-var welcomeMsg = document.querySelector('#welcome-message');
-var viewFavs = document.querySelector('#view-favorites');
 var favPage = document.querySelector('.favorites-page');
 var favsList = document.querySelector('ul');
+var heart = document.querySelector('#heart');
+var loginButton = document.querySelector('#login-button');
+var loginPage = document.querySelector('.login-page');
+var mainPage = document.querySelector('.main-page');
+var mantraButton = document.querySelector('#mantra');
+var message = document.querySelector('#message');
+var nameInput = document.querySelector('#login-box');
+var receiveMsgBtn =  document.querySelector('#receive-message');
 var returnButton = document.querySelector('#return');
 var savedHeader = document.querySelector('#fav-messages');
-var heart = document.querySelector('#heart');
+var viewFavs = document.querySelector('#view-favorites');
+var welcomeMsg = document.querySelector('#welcome-message');
 
+var affirmations = [
+  "I forgive myself and set myself free.",
+  "I believe I can be all that I want to be.",
+  "I am in the process of becoming the best version of myself.",
+  "I have the freedom & power to create the life I desire.",
+  "I choose to be kind to myself and love myself unconditionally.",
+  "My possibilities are endless.",
+  "I am worthy of my dreams.",
+  "I am enough.",
+  "I deserve to be healthy and feel good.",
+  "I am full of energy and vitality and my mind is calm and peaceful.",
+  "Every day I am getting healthier and stronger.",
+  "I honor my body by trusting the signals that it sends me.",
+  "I manifest perfect health by making smart choices.",
+];
 var mantras = [
   "Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
   "Do not let yesterday take up too much of today.",
@@ -32,23 +47,6 @@ var mantras = [
   "Onward and upward.",
   "I am the sky, the rest is weather."
 ];
-
-var affirmations = [
-  "I forgive myself and set myself free.",
-  "I believe I can be all that I want to be.",
-  "I am in the process of becoming the best version of myself.",
-  "I have the freedom & power to create the life I desire.",
-  "I choose to be kind to myself and love myself unconditionally.",
-  "My possibilities are endless.",
-  "I am worthy of my dreams.",
-  "I am enough.",
-  "I deserve to be healthy and feel good.",
-  "I am full of energy and vitality and my mind is calm and peaceful.",
-  "Every day I am getting healthier and stronger.",
-  "I honor my body by trusting the signals that it sends me.",
-  "I manifest perfect health by making smart choices.",
-]
-
 var savedMsgs = [];
 
 nameInput.addEventListener('click', removeDefault);
@@ -59,14 +57,28 @@ viewFavs.addEventListener('click', viewFavPage);
 returnButton.addEventListener('click', returnToMain);
 favPage.addEventListener('dblclick', removeMsg);
 
+function hide(element) {
+  element.classList.add('hidden');
+}
+
+function display(element) {
+  element.classList.remove('hidden');
+}
+
 function removeDefault() {
   nameInput.value = "";
 }
+
 function enterSite() {
   var name = nameInput.value;
-  mainPage.classList.remove('hidden');
-  loginPage.classList.add('hidden');
+  display(mainPage);
+  hide(loginPage);
+
+  if (name === "") {
+  welcomeMsg.innerText = `Hello, beautiful. Click below for some inspiration...`;
+  } else {
   welcomeMsg.innerText = `Hello, ${name}. Click below for some inspiration...`;
+ }
 }
 
 function getRandomIndex(array) {
@@ -81,22 +93,28 @@ function displayMessage() {
   } else {
     return;
   }
-  bellIcon.classList.add('hidden');
-  heart.classList.remove('hidden');
+  hide(bellIcon);
+  display(heart);
 }
 
 function saveMsg() {
   if (!savedMsgs.includes(message.innerText)) {
   savedMsgs.push(message.innerText);
-  viewFavs.classList.remove('hidden');
-}
+  display(viewFavs);
+  }
 }
 
 function viewFavPage() {
   var name = nameInput.value;
-  mainPage.classList.add('hidden');
-  favPage.classList.remove('hidden');
-  savedHeader.innerText = `${name}'s Favorite Messages`
+  hide(mainPage);
+  display(favPage);
+
+  if (name === "") {
+  savedHeader.innerText = `Favorite Messages`;
+  } else {
+  savedHeader.innerText = `${name}'s Favorite Messages`;
+  }
+
   favsList.innerHTML = '<ul></ul>';
   for (var i = 0; i < savedMsgs.length; i++) {
     favsList.insertAdjacentHTML(`beforeend`, `<li id=${i}>${savedMsgs[i]}</li>`);
@@ -104,8 +122,8 @@ function viewFavPage() {
 }
 
 function returnToMain() {
-  favPage.classList.add('hidden');
-  mainPage.classList.remove('hidden');
+  hide(favPage);
+  display(mainPage);
 }
 
 function removeMsg(event) {
